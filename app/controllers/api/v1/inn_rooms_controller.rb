6fe6ca3b4afb64d0e1a7632aed10171c1e6a3dd7
mbellucio <mbelluciocoding@gmail.com>
2024-05-29 15:11:49 -0300

    API: GET inn room details endpoint - /api/v1/inn_rooms/:id

--- app/controllers/api/v1/inn_rooms_controller.rb
+++ app/controllers/api/v1/inn_rooms_controller.rb
@@ -1,3 +1,10 @@
 class Api::V1::InnRoomsController < ActionController::API
-  
+  def show
+    begin
+      inn_room = InnRoom.find(params[:id])
+      render status: 200, json: inn_room.as_json(only: [:name, :size, :guest_limit, :daily_rate_cents])
+    rescue
+      render status: 404, json: {error: "Unable to find a room with the given id"}.to_json
+    end
+  end
 end
--- spec/requests/api/inn_rooms_api_spec.rb
+++ spec/requests/api/inn_rooms_api_spec.rb
@@ -27,7 +27,7 @@ describe 'Inn Room API' do
         name: 'Quarto Térreo',
         size: 30,
         guest_limit: 3,
-        daily_rate_cents: 10_000
+        daily_rate_cents: 10000
       )
       #act
       get "/api/v1/inn_rooms/#{inn_room.id}"
@@ -35,11 +35,46 @@ describe 'Inn Room API' do
       expect(response.status).to eq 200
       expect(response.content_type).to include "application/json"
       json_response = JSON.parse(response.body)
-      expect(json_response.length).to eq 1
-      expect(json_response.first["name"]).to eq inn_room.name
-      expect(json_response.first["size"]).to eq inn_room.size
-      expect(json_response.first["guest_limit"]).to eq inn_room.guest_limit
-      expect(json_response.first["daily_rate_cents"]).to eq inn_room.daily_rate_cents
+      expect(json_response["name"]).to eq inn_room.name
+      expect(json_response["size"]).to eq inn_room.size
+      expect(json_response["guest_limit"]).to eq inn_room.guest_limit
+      expect(json_response["daily_rate_cents"]).to eq inn_room.daily_rate_cents
+    end
+
+    it 'fails if id do not exists' do
+      #arrange
+      inn_owner = InnOwner.create!(
+        first_name: 'Joao',
+        last_name: 'Almeida',
+        document: '53783222001',
+        email: 'joao@email.com',
+        password: '123456'
+      )
+      inn = inn_owner.create_inn!(
+        name: 'Pousada do Almeidinha',
+        registration_number: '30638898000199',
+        description: 'Um bom lugar',
+        address_attributes: {
+          address: 'Rua X',
+          number: '100',
+          city: 'Manaus',
+          state: 'AM',
+          postal_code: '69067-080',
+          neighborhood: 'Centro'
+      })
+      inn_room = inn.inn_rooms.create!(
+        name: 'Quarto Térreo',
+        size: 30,
+        guest_limit: 3,
+        daily_rate_cents: 10_000
+      )
+      #act
+      get "/api/v1/inn_rooms/99999"
+      #assert
+      expect(response.status).to eq 404
+      expect(response.content_type).to include "application/json"
+      json_response = JSON.parse(response.body)
+      expect(json_response["error"]).to eq "Unable to find a room with the given id"
     end
   end
 end
